ce1475509928dc1d76160856274450f6edb55b95
Mokich <nokpsn2000@gmail.com>
2025-11-14 19:20:17 +0200

    puddle moves

--- game.js
+++ game.js
@@ -13,6 +13,20 @@ let game = {
     },
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
+        this.setEvents();
+    },
+    setEvents() {
+        window.addEventListener("keydown", e => {
+            if (e.keyCode === 37) {
+                this.platform.dx = -this.platform.velocity;
+            } else if (e.keyCode === 39) {
+                this.platform.dx = this.platform.velocity;
+            }
+        });
+        window.addEventListener("keyup", e => {
+            this.platform.dx = 0;
+        });
+
     },
     preload(callback) {
         let loaded = 0;
@@ -40,8 +54,12 @@ let game = {
             }
         }
     },
+    update(){
+        this.platform.move();
+    },
     run() {
         window.requestAnimationFrame(() => {
+            this.update();
             this.render();
             this.run();
         });
@@ -75,8 +93,15 @@ game.ball = {
 };
 
 game.platform = {
+    velocity: 6,
+    dx: 0,
     x: 280,
-    y: 300
+    y: 300,
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+        }
+    }
 };
 
 window.addEventListener("load", () => {
