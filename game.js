a1b0692b646da02544cd084f890ee61b73bc17c5
Oleg Konnov <oleg.konnov@ivkhk.ee>
2025-11-20 17:46:25 +0200

    random angle launch

--- game.js
+++ game.js
@@ -11,6 +11,8 @@ let game = {
     blocks: [],
     rows: 4,
     cols: 8,
+    width: 640,
+    height: 360,
     sprites: {
         background: null,
         ball: null,
@@ -32,7 +34,7 @@ let game = {
             
         });
         window.addEventListener("keyup", e => {
-            this.platform.dx = 0;
+            this.platform.stop();
         });
 
     },
@@ -74,6 +76,7 @@ let game = {
         });
     },
     render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
             this.ball.x, this.ball.y, this.ball.width, this.ball.height);
@@ -91,10 +94,14 @@ let game = {
             this.create();
             this.run();
         });
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 };
 
 game.ball = {
+    dx: 0,
     dy: 0,
     velocity: 3,
     x: 320,
@@ -103,10 +110,14 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity;
+        this.dx = game.random(-this.velocity, this.velocity);
     },
     move() {
         if (this.dy){
         this.y += this.dy;
+        }
+        if (this.dx) {
+            this.x += this.dx;
         }
     }
 };
@@ -120,7 +131,6 @@ game.platform = {
         fire() {
             if (this.ball) {
                 this.ball.start();
-        
                 this.ball = null;
         }
     },
