bae1e6c3d9ea435206e5e39a49e538c89625c3c9
Mokich <nokpsn2000@gmail.com>
2025-12-01 19:34:43 +0200

    add sound

--- game.js
+++ game.js
@@ -21,6 +21,9 @@ let game = {
         platform: null,
         block: null
     },
+    sounds: {
+        bump:null
+    },
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
         this.setEvents();
@@ -40,16 +43,29 @@ let game = {
     preload(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
-        let onImageLoad = () => {
+        required += Object.keys(this.sounds).length
+
+        let onResourceLoad = () => {
             ++loaded;
             if (loaded >= required) {
                 callback();
             }
         };
+
+        this.preloadSprites(onResourceLoad);
+        this.preloadAudio(onResourceLoad);
+    },
+    preloadSprites(onResourceLoad) {
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
-            this.sprites[key].addEventListener("load", onImageLoad);
+            this.sprites[key].addEventListener("load", onResourceLoad);
+        }
+    },
+    preloadAudio(onResourceLoad){
+        for (let key in this.sounds){
+            this.sounds[key] = new Audio("sounds/" + key + ".mp3");
+            this.sounds[key].addEventListener("canplaythrough", onResourceLoad, {once: true});
         }
     },
     create() {
@@ -182,12 +198,15 @@ game.ball = {
         if (ballLeft < worldLeft) {
             this.x = 0;
             this.dx = this.velocity;
+            game.sounds.bump.play();
         } else if (ballRight > worldRight) {
             this.x = worldRight - this.width;
             this.dx = -this.velocity;
+            game.sounds.bump.play();
         } else if (ballTop < worldTop) {
             this.y = 0;
             this.dy = this.velocity;
+            game.sounds.bump.play();
         } else if (ballBottom > worldBottom) {
             alert("Game Over");
         }
